1e0e01b883142dc1305147c61490eedd2153a22d
luye huang <luyeade@163.com>
2017-06-01 21:39:35 +0800

    initial commit2

--- src/js/components/channel.js
+++ src/js/components/channel.js
@@ -2,6 +2,8 @@
  * Created by luye on 2017/6/1.
  */
 import React from 'react';
+import {inject, observer} from 'mobx';
+@inject(['channelStore']) @observer
 export default class Channel extends React.Component{
   constructor(){
     super();
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -2,7 +2,11 @@
  * Created by luye on 2017/5/27.
  */
 import NewStore from "./new.js";
+import LawnStore from './lawn';
+import ChannelStore from './channel';
 
 export default {
-  newStore:new NewStore()
+  newStore:new NewStore(),
+  lawnStore: new LawnStore(),
+  channelStore: new ChannelStore()
 }
